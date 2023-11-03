8bcb956a381fb998c4da7090db8483c22415b1f3
Laurent Bihin <lolofanf1@hotmail.com>
2023-11-03 23:05:38 +0100

    remove unused path

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,11 +22,8 @@ from accounts import views as account_views
 urlpatterns = [
     path("", app_views.home_view, name='home'),
     path('gardens/', include('gardens.urls')),
-    # path('gardens/', garden_views.garden_search_view, name='search-garden'),
-    # path('gardens/create/', garden_views.garden_create_view, name='create-garden'),
-    # path('gardens/<slug:slug>/', garden_views.garden_details_view, name='details-garden'),
     path('admin/', admin.site.urls),
-    path('login/',account_views.login_view),
+    path('login/', account_views.login_view),
     path('logout/', account_views.logout_view),
     path('register/', account_views.register_view)
 ]
