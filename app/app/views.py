778d8a59377c89e0b18503e45c405be6656cee5b
Laurent Bihin <lolofanf1@hotmail.com>
2023-12-25 23:06:22 +0100

    Refactor home view and import Django authentication modules
    Replaced the function-based home view with a class-based view, using Django's generic TemplateView. Additionally, imported Django's authentication modules, including UserCreationForm and LoginView, to facilitate user registration and login. These changes will help optimize code structure and allow future enhancements in user authentication process.

--- app/app/views.py
+++ app/app/views.py
@@ -1,12 +1,5 @@
-from django.shortcuts import render
-
-from gardens.models import Garden
+from django.views.generic import TemplateView
 
 
-def home_view(request):
-    context = {}
-    garden_qs = Garden.objects.all()
-    context = {
-        'garden_list': garden_qs
-    }
-    return render(request, 'index.html', context=context)
+class IndexView(TemplateView):
+    template_name = 'index.html'
