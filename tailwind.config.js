ef7a7141d9455ebf47c7bc5737af5a7c79c0bf5d
Laurent Bihin <lolofanf1@hotmail.com>
2023-12-29 17:48:44 +0100

    Add tailwindcss to the project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
+
