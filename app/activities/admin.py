df0209ddc5c4d01ad49dc4952ec49128b4474805
Laurent Bihin <lolofanf1@hotmail.com>
2024-01-19 23:31:48 +0100

    Refactor fertilization model and update activity view

--- app/activities/admin.py
+++ app/activities/admin.py
@@ -6,8 +6,8 @@ from activities.models import Fertilization, Activity
 # Register your models here.
 
 class FertilizationAdmin(admin.ModelAdmin):
-    list_display = ('due_date', 'quantity_as_float')
-    search_fields = ('due_date', 'quantity_as_float')
+    list_display = ('quantity_as_float',)
+    search_fields = ('quantity_as_float',)
 
 
 # Register the model and admin class
@@ -15,8 +15,9 @@ admin.site.register(Fertilization, FertilizationAdmin)
 
 
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('activity', 'garden', 'fertilization')
+    list_display = ('creation', 'activity', 'garden', 'fertilization')
     search_fields = ('activity',)
+    readonly_fields = ('updated',)
 
 
 # Register the model and admin class
--- app/activities/models.py
+++ app/activities/models.py
@@ -8,15 +8,11 @@ from gardens.models import Garden
 
 
 class Fertilization(models.Model):
-    due_date = models.DateTimeField()
     quantity_as_float = models.FloatField(null=True, blank=True)
     unit = models.CharField(max_length=50, validators=[validate_unit_measurement], blank=True, null=True)
 
     def __str__(self):
-        return f"{self.due_date}: {self.quantity_as_float}{self.unit}"
-
-    def since_creation_timestamp(self):
-        return compute_time_difference(self.due_date)
+        return f"{self.quantity_as_float}{self.unit}"
 
     def convert_to_system(self, system="mks"):
         if self.quantity_as_float is None:
@@ -26,8 +22,11 @@ class Fertilization(models.Model):
 
     def as_mks(self):
         # meter, kilogram, second
-        measurement = self.convert_to_system(system='mks')
-        return "{:~}".format(measurement.to_base_units())
+        measurement = self.convert_to_system(system='mks').to_base_units()
+        number = measurement.magnitude
+        unit = measurement.units
+        formatted_number = "{:,.2f}".format(number).rstrip('0').rstrip('.').replace(",", ".")
+        return "{} {:~}".format(formatted_number, unit)
 
     def get_quantity(self, system="mks"):
         if system == "mks":
@@ -52,6 +51,8 @@ class ActivityChoices(models.TextChoices):
 
 
 class Activity(models.Model):
+    creation = models.DateTimeField()
+    updated = models.DateTimeField(auto_now=True)
     activity = models.CharField(max_length=1, choices=ActivityChoices.choices)
     comment = models.TextField()
     garden = models.ForeignKey(Garden, on_delete=models.CASCADE, related_name='activities', null=True, blank=True)
@@ -59,6 +60,15 @@ class Activity(models.Model):
 
     def __str__(self):
         return self.activity
+
+    def get_creation_date(self):
+        return self.creation.strftime('%d.%m.%y')
+
+    def get_updated_date(self):
+        return self.updated.strftime('%d.%m.%y')
+
+    def since_update(self):
+        return compute_time_difference(self.updated)
 
     def get_children_activity(self):
         if self.fertilization:
